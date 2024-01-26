592a6ce8320e2aac1af368711847233bd3faca3f
Cheikh Ahmed Tidiane Gueye <cheikhahmedtidiane219@gmail.com>
2024-01-26 13:03:02 +0000

    specification cors()

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,6 @@ require("dotenv").config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
-
-app.use("/api", authRoutes);
-
-const port = process.env.PORT || 5000;
 
 // Connexion à la base de donnée
 mongoose.connect(process.env.MONGO_URI).then((result) => {
@@ -21,6 +16,17 @@ mongoose.connect(process.env.MONGO_URI).then((result) => {
   console.error("MongoDB connection error:", error);
   process.exit(1);
 });
+
+app.use(cors());
+app.use(
+  cors({
+    origin: "https://red-product.vercel.app",
+  })
+);
+
+app.use("/api", authRoutes);
+
+const port = process.env.PORT || 5000;
 
 
 // Lancer le serveur
